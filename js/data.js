0fb0bc41becdc64cd9dff562fe9770846e018481
DangVanLy <lydang1000@gmail.com>
2025-01-27 00:53:08 -0500

    first commit

---
+++ js/data.js
@@ -0,0 +1,145 @@
+// Số điện thoại studio
+export const phoneNumber = '0763 91 95 92'
+
+// Địa chỉ studio
+export const location = {
+    address: '197L Trần Hưng Đạo, Phường 5, Vị Thanh, Hậu Giang 95107, Việt Nam',
+    linkGoogleMap: 'https://www.google.com/maps/place/Alys+tattoo/@9.7886952,105.4707373,16.8z/data=!4m16!1m9!3m8!1s0x31a095cd05ff3b13:0x890f542776f605a7!2sAlys+tattoo!8m2!3d9.78886!4d105.4730565!9m1!1b1!16s%2Fg%2F11svnycyg0!3m5!1s0x31a095cd05ff3b13:0x890f542776f605a7!8m2!3d9.78886!4d105.4730565!16s%2Fg%2F11svnycyg0?hl=vi&entry=ttu&g_ep=EgoyMDI1MDEwNi4xIKXMDSoASAFQAw%3D%3D',
+    miniMap: '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3931.7304502291977!2d105.4730565!3d9.78886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a095cd05ff3b13%3A0x890f542776f605a7!2sAlys%20tattoo!5e0!3m2!1svi!2ssg!4v1736423887892!5m2!1svi!2ssg" width="360" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+}
+
+// Danh sách các mạng xã hội
+export const socials = [
+    {
+        link: 'https://www.instagram.com/alys.tattoo',
+        icon: 'ti-instagram'
+    },
+    {
+        link: 'https://www.facebook.com/tattoovithanh',
+        icon: 'ti-facebook'
+    },
+    {
+        link: 'https://www.tiktok.com/@alys_tattoo',
+        icon: null
+    }
+]
+
+// Quy trình chill
+export const process = [
+    {
+        id: 1,
+        title: ' Tư vấn "siêu chill"',
+        content: 'Nhắn tin hoặc ghé ngay studio để bàn ý tưởng với tụi mình. Mang vibe của bạn tới, và tụi mình sẽ giúp nó thành hình thật chất!'
+    },
+    {
+        id: 2,
+        title: 'Đặt lịch “quẩy mực”',
+        content: 'Quyết xong ý tưởng? Ok, tụi mình báo giá siêu hợp lý để bạn book lịch liền tay và chốt luôn ngày hành động!'
+    },
+    {
+        id: 3,
+        title: 'Xăm thôi nào!',
+        content: 'Tới studio, tụi mình đã chuẩn bị sẵn thiết kế để bạn "chill". Cứ thư giãn và tận hưởng hành trình nghệ thuật mới trên da bạn!'
+    }
+]
+
+//Đánh giá từ khách hàng
+export const customerReviews = [
+    {
+        name: 'Phương Quyên Lâm',
+        content: 'Hình xăm đẹp giá cả hợp lí, anh thợ xăm dui vẻ, 10đ nhó hẹn lần sau quay lại😸 …',
+        avatar: 'https://lh3.googleusercontent.com/a-/ALV-UjVTFqU4l_X8H9bTni_CeF1PqGo2-JBxQPL9FHFvHpwUx7eFeyk=w36-h36-p-rp-mo-br100',
+        linkGoogleMap: 'https://maps.app.goo.gl/binpEnwSTF2TwuwL8'
+    },
+    {
+        name: 'An BarBer',
+        content: 'Thợ xăm vui vẻ, xăm rất tỉ mỉ quá ok.',
+        avatar: 'https://lh3.googleusercontent.com/a-/ALV-UjVhGQkDZNrglALjkbeyg6n_4IyFnvu3hrZL_vhHNtj6GIvSO0BX=w120-h120-p-rp-mo-br100',
+        linkGoogleMap: 'https://maps.app.goo.gl/Fa946DdKC8euQL5T7'
+    }
+]
+
+//Thời gian làm việc
+export const workingHour = [
+    {name: 'Thứ 2', time: '10:00 - 20:00'},
+    {name: 'Thứ 3', time: '10:00 - 20:00'},
+    {name: 'Thứ 4', time: '10:00 - 20:00'},
+    {name: 'Thứ 5', time: '10:00 - 20:00'},
+    {name: 'Thứ 6', time: '10:00 - 20:00'},
+    {name: 'Cuối tuần', time: 'Đóng cửa'}
+]
+
+//Album chất chơi studio
+export const album = [
+    {
+        fileName: '4.webp'
+    }, {
+        fileName: '10.webp'
+    }, {
+        fileName: '1.webp'
+    }, {
+        fileName: '2.webp'
+    }, {
+        fileName: '5.webp'
+    }
+]
+
+// Bảng giá dịch vụ
+export const priceList = [
+    {
+        name: 'Xăm hình nghệ thuật',
+        desc: 'Siêu nghệ với hình xăm độc lạ, thể hiện chất riêng trên cơ thể! 🎨',
+        price: 'chỉ từ 600.000 ₫'
+    },
+    {
+        name: 'Cover hình',
+        desc: 'Che phủ hoàn hảo, hô biến những hình cũ thành siêu phẩm mới! 🔥',
+        price: 'chỉ từ 450.000 ₫'
+    },
+    {
+        name: 'Xỏ khuyên cá tính',
+        desc: 'Đa phong cách, xịn mịn, khuyên nào cũng chất! 🛠️',
+        price: 'chỉ từ 300.000 ₫'
+    },
+    {
+        name: 'Xóa xăm bằng Laser',
+        desc: 'Xóa mực chuẩn an toàn, trả lại làn da fresh như ngày nào! ✨',
+        price: 'Liên hệ'
+    },
+]
+
+// Danh sách artist
+export const artists = [
+    {
+        name: 'Lý Đặng',
+        avatar: 'DangVanLy.webp',
+        job: 'Tattoo & Piercing Artist',
+        desc: 'Thợ xăm chính hiệu, chuyên nghiệp và tận tâm. Hãy để Alys giúp bạn thể hiện phong cách riêng trên cơ thể!',
+        phone_number: '0763 919 592',
+        link_profile: 'DangVanLy-details.html',
+        socials: [
+            {
+                link: 'https://www.instagram.com/alys.tattoo',
+                icon: 'ti-instagram'
+            },
+            {
+                link: 'https://www.facebook.com/alystattoo',
+                icon: 'ti-facebook'
+            }
+        ]
+    },
+    {
+        name: 'Mỹ Diệu',
+        avatar: 'MyDieu.webp',
+        job: 'Cosmetic Tattooing & Tattoo Removal',
+        desc: 'Phun xăm chuẩn gu, xoá xăm chuẩn chất. Làm đẹp thần thái là việc của mình, chill là việc của bạn! ✨',
+        phone_number: '0796 973 091',
+        link_profile: 'MyDieu-details.html',
+        socials: [
+            {
+                link: 'https://www.facebook.com/angdieu.865229',
+                icon: 'ti-facebook'
+            }
+        ]
+    }
+]
